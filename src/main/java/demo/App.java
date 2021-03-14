72304824d1d58001c127d40229536ac09f1ba98f
amruta-nadgonde <amruta-nadgonde@gmail.com>
2021-03-14 15:51:09 +0530

    first commit

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class App {
+
+	public static int add (int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int sub (int a, int b)
+	{
+		return a-b;
+	}
+
+}
---
+++ src/test/java/tests/AppTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import demo.App;
+
+public class AppTest {
+  @Test
+  public void addTest() {
+	  
+	  int exp = 10;
+	  int act = App.add(8, 2);
+	  assertEquals(act, exp);
+  }
+  
+  @Test
+  public void subTest() {
+	  
+	  int exp = 10;
+	  int act = App.sub(18, 8);
+	  assertEquals(act, exp);
+  }
+}
